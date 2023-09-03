c67220ef118edb4dcfec5eb2aaa33d424ba91f2f
ashloverscn <ashloverscn@gmail.com>
2023-09-03 06:13:44 +0530

    Create part4.a.compile-and-install-asterisk-perl.sh

---
+++ part4.a.compile-and-install-asterisk-perl.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#https://github.com/asterisk-perl/asterisk-perl
+#https://metacpan.org/dist/asterisk-perl
+
+echo -e "\e[0;32m Install Asterisk Perl v0.08 \e[0m"
+sleep 2
+cd /usr/src
+wget http://download.vicidial.com/required-apps/asterisk-perl-0.08.tar.gz
+tar xzf asterisk-perl-0.08.tar.gz
+cd asterisk-perl-0.08
+perl Makefile.PL
+make all
+make install
+
+
