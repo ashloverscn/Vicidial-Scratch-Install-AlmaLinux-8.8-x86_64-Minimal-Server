0668693759f3f8f4054f588a8a0ee83b2bd4f562
ashloverscn <ashloverscn@gmail.com>
2024-08-03 03:29:14 +0530

    Update part0.system-hw-prepration.sh

--- part0.system-hw-prepration.sh
+++ part0.system-hw-prepration.sh
@@ -49,7 +49,7 @@ firewall-cmd --reload
 echo -e "\e[0;32m Enable cockpit root access and port 9090 in firewalld zone public \e[0m"
 sleep 2
 
-sed -i 's/#root/root/g' /etc/cockpit/disallowed-users
+sed -i 's/root/#root/g' /etc/cockpit/disallowed-users
 systemctl enable --now cockpit.socket
 
 echo -e "\e[0;32m Enable grub verbose boot \e[0m"
