433c042ee6784e5a90f20c5584276e54c77a4ecc
ashloverscn <ashloverscn@gmail.com>
2025-04-21 23:44:59 +0530

    Update part3.install-perl-cpan-cpanm-modules.sh

--- part3.install-perl-cpan-cpanm-modules.sh
+++ part3.install-perl-cpan-cpanm-modules.sh
@@ -2,6 +2,7 @@
 
 echo -e "\e[0;32m Install and Configure Perl-CPAN\Perl-CPAN-Modules \e[0m"
 sleep 2
+cd /usr/src
 yum install -y perl-CPAN perl-YAML perl-CPAN-DistnameInfo perl-libwww-perl perl-DBI perl-DBD-MySQL perl-GD perl-Env perl-Term-ReadLine-Gnu perl-SelfLoader perl-open.noarch 
 echo -e "\e[0;32m All cpan-modules installed and verified successfuly \e[0m"
 sleep 2
