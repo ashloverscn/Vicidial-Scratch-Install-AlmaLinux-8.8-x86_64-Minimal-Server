6fc9a9c3f8c71621918941458b97f23bd9bdd458
ashloverscn <ashloverscn@gmail.com>
2025-04-23 07:08:14 +0530

    Update part3.install-perl-cpan-cpanm-modules.sh

--- part3.install-perl-cpan-cpanm-modules.sh
+++ part3.install-perl-cpan-cpanm-modules.sh
@@ -5,7 +5,7 @@ sleep 2
 cd /usr/src
 yum install -y perl-CPAN perl-YAML perl-CPAN-DistnameInfo perl-libwww-perl perl-DBI perl-DBD-MySQL perl-GD perl-Env perl-Term-ReadLine-Gnu perl-SelfLoader perl-open.noarch 
 #CPM install
-mkdir /usr/src
+cd /usr/src
 curl -fsSL https://raw.githubusercontent.com/skaji/cpm/main/cpm | perl - install -g App::cpm
 /usr/local/bin/cpm install -g
 echo -e "\e[0;32m All cpan-modules installed and verified successfuly \e[0m"
