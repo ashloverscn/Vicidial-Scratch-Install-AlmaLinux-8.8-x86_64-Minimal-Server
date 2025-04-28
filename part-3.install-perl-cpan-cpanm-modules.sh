16f995edb78b9029695251eea7f9d079fcff4aee
ashloverscn <ashloverscn@gmail.com>
2025-04-28 13:46:52 +0530

    Update part-3.install-perl-cpan-cpanm-modules.sh

--- part-3.install-perl-cpan-cpanm-modules.sh
+++ part-3.install-perl-cpan-cpanm-modules.sh
@@ -4,24 +4,22 @@ echo -e "\e[0;32m Uninstall and remove Perl-CPAN\Perl-CPAN-Modules \e[0m"
 sleep 2
 cd /usr/src
 yum remove -y perl-CPAN perl-YAML perl-CPAN-DistnameInfo perl-libwww-perl perl-DBI perl-DBD-MySQL perl-GD perl-Env perl-Term-ReadLine-Gnu perl-SelfLoader perl-open.noarch 
+##Get the current Perl version dynamically
+perl_version=$(perl -V:version | awk -F= '{print $2}' | tr -d ' "')
 cpm --clean
 ##CPM complete uninstall remove
 rm -f $(which cpm)
 ##Remove CPM-related cache and configuration
 rm -rf ~/.perl-cpm
-##Remove Perl libraries from system-installed directories
-rm -rf /usr/local/lib/*/perl/*
-rm -rf /usr/local/share/perl/*
 #Remove the CPM executable from common installation locations
 rm -rf /usr/local/bin/cpm
 rm -rf /bin/cpm
 #Remove CPAN and CPAN Minus caches
 rm -rf ~/.cpanm
 rm -rf ~/.cpan
-##Get the current Perl version dynamically
-perl_version=$(perl -V:version | awk -F= '{print $2}' | tr -d ' "')
-##Remove CPM executable
-rm -f /usr/local/bin/cpm
+# Remove Perl libraries from system-installed directories for the current Perl version
+rm -rf /usr/local/lib/$perl_version/perl/*
+rm -rf /usr/local/share/perl/$perl_version/*
 ##Remove CPM related Perl modules (dynamically for any version)
 rm -rf /usr/local/share/perl5/$perl_version/App/cpm
 rm -f /usr/local/share/perl5/$perl_version/App/cpm.pm
